f848f81522fb5997e0fa36ac6607195a87dc65c1
HelChris <111705707+HelChris@users.noreply.github.com>
2024-01-20 20:57:12 +0100

    from innerHTML to createElement

--- js/ui/games/createGames.js
+++ js/ui/games/createGames.js
@@ -2,13 +2,39 @@ export function createGames(container, games) {
 	container.innerHTML = "";
 
 	games.forEach(function (game) {
-		container.innerHTML += `<div class="gamecard">
-		  <img src="${game.image}" class="image-size" alt="${game.title}" />
-		  <h3>${game.title}</h3>
-			<p>$${game.price}</p>
-			<p>${game.genre}</p>
-		  <a href="gamedetail.html?id=${game.id}" class="button button-white">Read more</a>
-			<a href="cart.html" class="button button-turquoise">Add to cart</a>
-			</div>`;
+		const gameCard = document.createElement("div");
+		gameCard.className = "gamecard";
+
+		const img = document.createElement("img");
+		img.src = game.image;
+		img.className = "image-size";
+		img.alt = game.title;
+		gameCard.appendChild(img);
+
+		const title = document.createElement("h3");
+		title.textContent = game.title;
+		gameCard.appendChild(title);
+
+		const price = document.createElement("p");
+		price.textContent = "$" + game.price;
+		gameCard.appendChild(price);
+
+		const genre = document.createElement("p");
+		genre.textContent = game.genre;
+		gameCard.appendChild(genre);
+
+		const readMore = document.createElement("a");
+		readMore.href = "gamedetail.html?id=" + game.id;
+		readMore.className = "button button-white";
+		readMore.textContent = "Read more";
+		gameCard.appendChild(readMore);
+
+		const addToCart = document.createElement("a");
+		addToCart.href = "cart.html";
+		addToCart.className = "button button-turquoise";
+		addToCart.textContent = "Add to cart";
+		gameCard.appendChild(addToCart);
+
+		container.appendChild(gameCard);
 	});
 }
