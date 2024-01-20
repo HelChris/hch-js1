c5c88a25d449405cd4cd0d5001d9d402aed89250
HelChris <111705707+HelChris@users.noreply.github.com>
2024-01-20 21:20:57 +0100

    from innerHTML to createElement

--- js/index.js
+++ js/index.js
@@ -4,31 +4,63 @@ async function getGames() {
 	try {
 		const response = await fetch(url);
 
-		if (response.ok !== true) {
-			throw new Error(`HTTP Error! status: ${response.status}`);
+		if (!response.ok) {
+			throw new Error("HTTP Error! status: ${response.status}");
 		}
 
 		const games = await response.json();
-
 		const resultsContainer = document.querySelector("#container");
 		resultsContainer.innerHTML = "";
 
-		games.forEach(function (game) {
-			resultsContainer.innerHTML += `<section class="product-list">
-			<div class="game-card">
-			<img src="${game.image}" class="image-size" alt="${game.title}" />
-			<h3>${game.title}</h3>
-			<p>$${game.price}</p>
-			<p>${game.genre}</p>
-			<a href="gamedetail.html?id=${game.id}" class="button button-white">Read more</a>
-			<a href="cart.html" class="button button-turquoise">Add to cart</a>
-			</div>
-			</section>`;
+		games.forEach((game) => {
+			const section = document.createElement("section");
+			section.className = "product-list";
+			resultsContainer.appendChild(section);
+
+			const gameCard = document.createElement("div");
+			gameCard.className = "game-card";
+			section.appendChild(gameCard);
+
+			const img = document.createElement("img");
+			img.src = game.image;
+			img.className = "image-size";
+			img.alt = game.title;
+			gameCard.appendChild(img);
+
+			const title = document.createElement("h3");
+			title.textContent = game.title;
+			gameCard.appendChild(title);
+
+			const price = document.createElement("p");
+			price.textContent = `$${game.price}`;
+			gameCard.appendChild(price);
+
+			const genre = document.createElement("p");
+			genre.textContent = game.genre;
+			gameCard.appendChild(genre);
+
+			const readMore = document.createElement("a");
+			readMore.href = `gamedetail.html?id=${game.id}`;
+			readMore.className = "button button-white";
+			readMore.textContent = "Read more";
+			gameCard.appendChild(readMore);
+
+			const addToCart = document.createElement("a");
+			addToCart.href = "cart.html";
+			addToCart.className = "button button-turquoise";
+			addToCart.textContent = "Add to cart";
+			gameCard.appendChild(addToCart);
 		});
 	} catch (error) {
+		console.error("Error fetching games:", error);
 		const resultsContainer = document.querySelector("#container");
-		resultsContainer.innerHTML =
-			'<p class="error">Oh no! An error occurred when loading the games.It will be fixed.</p>';
+		resultsContainer.innerHTML = "";
+		const errorParagraph = document.createElement("p");
+		errorParagraph.className = "error";
+		errorParagraph.textContent =
+			"Oh no! An error occurred when loading the games.";
+		resultsContainer.appendChild(errorParagraph);
 	}
 }
+
 getGames();
