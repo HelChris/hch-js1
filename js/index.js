7aa68235d126d4c3b20c181db9aec7521b0be4db
HelChris <111705707+HelChris@users.noreply.github.com>
2024-01-20 19:06:17 +0100

    update

--- js/index.js
+++ js/index.js
@@ -1,14 +1,17 @@
 import { getGames } from "./api/getGames.js";
 import { displayGames } from "./ui/games/displayGames.js";
-import { displayErrorMessage} from "./ui/shared/displayMessage.js";
+import { displayErrorMessage } from "./ui/shared/displayMessage.js";
 
 async function getGames() {
 	try {
 		const games = await getGames();
 		displayGames(games, "#container");
 	} catch (error) {
-		console.error('error getting games:' error);
-		displayErrorMessage("#container", "Oh no! An error occured when loading the games. It will be fixed.");
+		console.error("error getting games:", error);
+		displayErrorMessage(
+			"#container",
+			"Oh no! An error occured when loading the games. It will be fixed."
+		);
 	}
 }
 getGames();
